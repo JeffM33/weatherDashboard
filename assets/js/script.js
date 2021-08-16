022c34d278bb80e31558e906da4b5deb9484947c
Jeffrey McLeod <jeffreymmcleod@outlook.com>
2021-08-16 09:08:01 -0500

    added css, html elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 let apiKey = "feaa8b1d4d25106844878c22085c20a6";
-let queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=Dallas&appid=${apiKey}`
+let queryUrl = `https://api.openweathermap.org/data/2.5/forecast?q=Dallas&appid=${apiKey}`
 
 fetch(queryUrl)
 .then(headers => headers.json())
